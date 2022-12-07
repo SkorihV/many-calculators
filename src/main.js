295fb3c9edffebf4f0b521d93ce8052c4af6e085
SkorihV <skorih.vitalii@gmail.com>
2022-12-07 09:16:37 +0700

    Удалены файлы первых калькуляторов, роутер. Корневой файл конструктора перенесен в App

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import {createPinia} from "pinia";
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "@/router";
 import store from "@/store/store";
 
 const AppCalc = createApp(App);
 
 AppCalc.config.unwrapInjectedRef = true;
-AppCalc.use(router);
 AppCalc.use(store);
 AppCalc.use(createPinia());
 AppCalc.mount("#App");
