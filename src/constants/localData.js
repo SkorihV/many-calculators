f712dfe86bf4a802a7fc283032ca33b0042ba54c
SkorihV <skorih.vitalii@gmail.com>
2023-12-12 17:52:46 +0700

    Убрал проверку для системной переменной на вывод данных

--- src/constants/localData.js
+++ src/constants/localData.js
@@ -1,3 +1,3 @@
 export const IS_LOCAL = process.env.NODE_ENV === 'development'
-export const LOCAL_PATH_DATA = "http://localhost:5000/data-test";
-export const LOCAL_PATH_OPTIONS = "http://localhost:5000/options-test";
+export const LOCAL_PATH_DATA = "http://localhost:5000/data-carloan";
+export const LOCAL_PATH_OPTIONS = "http://localhost:5000/options-carloan";
